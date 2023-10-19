46263f66a48f1fdb48ea1a9d61138e4d084270ad
hammtah <115116715+hammtah@users.noreply.github.com>
2023-10-19 22:21:58 +0200

    Added files

---
+++ data.js
@@ -0,0 +1,100 @@
+export const foods = [{
+        name: "Pizza",
+        ingredients: ["pepperoni", " mushrom", " mozarella"],
+        image: "images/pizza.png",
+        price: 3,
+        id: 1,
+        repeated: 0
+
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        image: "images/burger.png",
+        price: 1.3,
+        id: 2,
+        repeated: 0
+
+    },
+    {
+        name: "Chinese food",
+        ingredients: ["grain", " hops", " yeast", " water"],
+        image: "images/chinese-food.png",
+        price: 2,
+        id: 3,
+        repeated: 0
+
+    },
+    {
+        name: "Mexican food",
+        ingredients: ["soya", " avocado", " Spenach", " Tomato"],
+        image: "images/mexican.png",
+        price: 7,
+        id: 4,
+        repeated: 0
+    },
+    {
+        name: "Salade plate ",
+        ingredients: ["Tomato", " olive", " onion", " soya"],
+        image: "images/plate.png",
+        price: 5,
+        id: 5,
+        repeated: 0
+    },
+    {
+        name: "Taco",
+        ingredients: ["sauce", " veget", " secret ingredient"],
+        image: "images/taco.png",
+        price: 10,
+        id: 6,
+        repeated: 0
+    }
+]
+
+
+
+
+// 
+
+
+export const juices = [{
+        name: "orange",
+        ingredients: ["orange", " water", " lemon"],
+        image: "images/juices/orange1.png",
+        price: 1.5,
+        id: 7,
+        repeated: 0
+    },
+    {
+        name: "barba juice",
+        ingredients: ["barba", " orange", " sugar"],
+        image: "images/juices/barba.png",
+        price: 1.7,
+        id: 8,
+        repeated: 0
+    },
+    {
+        name: "khiar juice",
+        ingredients: ["khiar", " fgouss", " water"],
+        image: "images/juices/khiar.png",
+        price: 2,
+        id: 9,
+        repeated: 0
+    },
+    {
+        name: "lemon juice",
+        ingredients: ["lemon", " water", " sprite"],
+        image: "images/juices/lemon.png",
+        price: 0.8,
+        id: 10,
+        repeated: 0
+    },
+    {
+        name: "carrot juice",
+        ingredients: ["carrot", " orange", " lemon"],
+        image: "images/juices/carrot.png",
+        price: 2,
+        id: 11,
+        repeated: 0
+    },
+]
---
+++ script.js
@@ -0,0 +1,193 @@
+import { foods, juices } from './data.js';
+
+let ordersArray = [];
+
+//chosed will stock the categorie chosed
+let chosed = "";
+
+renderChosedRadio();
+
+document.querySelector(".select").addEventListener("change", function() {
+    renderChosedRadio();
+})
+
+
+function renderChosedRadio() {
+    const chosedCategorie = document.querySelector("input[type='radio']:checked");
+    if (chosedCategorie.id === "foods") {
+        render(foods);
+        chosed = foods;
+    } else if (chosedCategorie.id === "juices") {
+        render(juices);
+        chosed = juices;
+    }
+}
+
+
+function render(categorie) {
+    let renderString = "";
+    for (const item of categorie) {
+        renderString += `
+                        <section class="items">
+                            <div class="img-section">
+                                <img src="${item.image}" alt="${item.name}" >
+                            </div>
+                            <div class="item-details">
+                                <div class="details-section">
+                                    <h3 class="title">${item.name}</h3>
+                                    <p class="ingredients lite">${item.ingredients}</p>
+                                    <p class="price">$${item.price}</p>
+                                </div>
+                                <div class="add-btn-section">
+                                    <button class="add" data-add="${item.id}"> +</button>
+                                </div>
+                            </div>
+                        </section>
+   `
+    }
+    document.querySelector(".rendering-section").innerHTML = renderString;
+
+}
+
+
+document.addEventListener("click", function(e) {
+    if (e.target.dataset.add) {
+        let clickedItem = whoClicked(e, chosed);
+        addingOrdersToArray(clickedItem, ordersArray);
+    } else if (e.target.dataset.remove) {
+        let clickedItem = handleRemoves(e);
+        clickedItem.repeated--;
+    }
+
+    renderOrderedItems(ordersArray);
+    renderTotalPrice(ordersArray);
+
+});
+
+function whoClicked(e, categorie) {
+    const clickedItem = categorie.filter(function(item) {
+        return (item.id == e.target.dataset.add);
+    })[0]
+    return clickedItem;
+}
+
+function addingOrdersToArray(clickedItem, ordersArray) {
+    if (!ordersArray.includes(clickedItem)) {
+        ordersArray.push(clickedItem);
+    }
+    clickedItem.repeated++;
+
+}
+
+function renderOrderedItems(ordersArray) {
+    let ordersString = "";
+    for (const order of ordersArray) {
+        if (order.repeated > 0) {
+            ordersString += `
+                        <div class="orders" data-remove="${order.id}">
+                            <p data-remove="${order.id}"><span class="repeated">${order.repeated}</span> ${order.name}</p>
+                            <p class="price" data-remove="${order.id}">$${order.price}</p>
+                        </div>
+                        `
+        }
+    }
+    document.querySelector(".orders-section").innerHTML = ordersString;
+}
+
+
+
+function renderTotalPrice(ordersArray) {
+    let result = 0;
+    ordersArray.forEach(function(order) {
+        result += (order.price * order.repeated);
+    });
+    document.querySelector(".total-price").innerHTML = `$${result.toFixed(1)}`;
+}
+
+
+
+function handleRemoves(e) {
+    // concatArrays is the concatination of all categories arrays (if you want to add another categorie use: foods.concat(juices,array3,array4) and so one)
+    const concatArrays = foods.concat(juices);
+    const clickedItem = concatArrays.filter(function(item) {
+        return (item.id == e.target.dataset.remove);
+    })[0]
+    return clickedItem;
+}
+
+
+
+document.querySelector(".up").addEventListener("click", function showPriceBar() {
+    document.querySelector("main").classList.toggle("padd-bot");
+    document.querySelector(".fa-caret-up").classList.toggle("hide-me");
+    document.querySelector(".fa-caret-down").classList.toggle("hide-me");
+    setTimeout(() => {
+        document.querySelector(".show-or-hide").classList.toggle("hide-me");
+    }, 100);
+})
+
+document.querySelector(".complete-order").addEventListener("click", function callTheForm() {
+    document.querySelector(".last-form").classList.remove("hide-me");
+    hideDivs();
+})
+
+
+document.querySelector(".submit").addEventListener("click", function(e) {
+    e.preventDefault();
+    //declarations
+    const name = document.querySelector("#name");
+    const cardNum = document.querySelector("#card-num");
+    const cvv = document.querySelector("#cvv");
+    const form = document.querySelector(".last-form");
+    //grabing the data if it's there
+    if (isEmpty(name.value) || isEmpty(cardNum.value) || isEmpty(cvv.value)) return;
+    let formData = getFormData(form);
+    //clearing the input fields
+    name.value = "";
+    cardNum.value = "";
+    cvv.value = "";
+    //showing the validation message 
+    renderMessg(formData);
+    document.querySelector(".last-form").classList.add("hide-me");
+})
+
+function getFormData(form) {
+    data = new FormData(form);
+    return {
+        name: data.get("name"),
+        cardNum: data.get("card-num"),
+        cvv: data.get("cvv")
+    }
+}
+
+function renderMessg(formData) {
+    const msg = document.createElement("div");
+    msg.classList.add("msg");
+    msg.classList.add("notpop");
+    document.querySelector("body").append(msg);
+    msg.innerHTML = `
+     <p> Thanks, ${formData.name}! Your order is on its way!</p>
+     `;
+    setTimeout(() => {
+        msg.classList.add("pop");
+    }, 300);
+
+}
+
+
+function isEmpty(str) {
+    return (!str.trim().length);
+}
+
+
+function showDivs() {
+    document.querySelector("header").classList.remove("hide-me");
+    document.querySelector("main").classList.remove("hide-me");
+    document.querySelector("footer").classList.remove("hide-me");
+}
+
+function hideDivs() {
+    document.querySelector("header").classList.add("hide-me");
+    document.querySelector("main").classList.add("hide-me");
+    document.querySelector("footer").classList.add("hide-me");
+}
